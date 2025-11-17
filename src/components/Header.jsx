d9357f33e06db0429701e0c07871ea4ae0e4231d
Leo Saban <leodsaban@gmail.com>
2025-11-17 08:17:47 -0700

    Finished Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// Header component
+export default function Header() {
+
+    return (
+        <header className="bg-blue-300 text-white pt-6 pb-4 text-shadow-sky-300">
+            <div className="max-w-6xl mx-auto px-4 text-center">
+                <h1 className="text-4xl font-bold mb-2">Photo Gallery</h1>
+                <p className="text-sm">
+                    Discover beautiful images organized by categories
+                </p>
+            </div>
+        </header>
+    )
+}
