629961e10b63ba96b34bdf700024cd90dc348139
Leo Saban <leodsaban@gmail.com>
2025-11-17 08:34:55 -0700

    finished CategoryFilter component

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,1 +1,19 @@
-// CategoryFilter component
+
+export default function CategoryFilter() {
+    const categories = ['All', 'Birds', 'Technology', 'Space', 'Cars']
+
+    return (
+
+        <section className="bg-gray-100 py-8">
+            <div className="max-w-6xl mx-auto px-8">
+                <div className="flex justify-center gap-4 flex-wrap">
+                    {categories.map((category) => (
+                        <button key={category}>
+                            {category}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
